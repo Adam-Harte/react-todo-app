053dcfa1995c2ae2d3f780a5754d173fde2f19e5
Adam-harte <adamharte11@hotmail.co.uk>
2018-11-05 16:33:20 +0000

    Navbar and NavItem componentns scaffolded with props defined and styling implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import Navbar from './Components/Navigation/Navbar/Navbar';
 
 class App extends Component {
   render() {
+    let routes = (
+      <Switch>
+        <Route path="/completed" Component={null} />
+        <Route path="/" Component={null} />
+      </Switch>
+    )
+
     return (
       <div className="App">
-
+        <Navbar />
+        {routes}
       </div>
     );
   }
---
+++ src/Components/Navigation/NavItem/NavItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './NavItem.css';
+
+const NavItem = (props) => {
+   return (
+      <li className="NavItem">
+         <NavLink to={props.path} exact activeClassName="active">{props.label}</NavLink>
+      </li>
+   );
+};
+
+export default NavItem;
