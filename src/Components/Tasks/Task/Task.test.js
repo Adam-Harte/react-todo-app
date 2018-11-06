e54de7356bc7222c6871d5885b1dee51c5e4f97f
Adam-harte <adamharte11@hotmail.co.uk>
2018-11-06 15:32:20 +0000

    Modal component added to Task component with conditional rendering and new test added to test the Modal conditional rendering

--- src/Components/Tasks/Task/Task.test.js
+++ src/Components/Tasks/Task/Task.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Task from './Task';
 import Button from '../../UI/Button/Button';
+import Modal from '../../UI/Modal/Modal';
 
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -23,5 +24,10 @@ describe('<Task />', () => {
    it('should render three Button component when the complete, edit and delete props are true', () => {
       wrapper.setProps({complete: true, edit: true, delete: true});
       expect(wrapper.find(Button)).toHaveLength(3);
+   });
+
+   it('should render a Modal component when the editModal state is true', () => {
+      wrapper.setState({editModal: true});
+      expect(wrapper.find(Modal)).toHaveLength(1);
    });
 });
